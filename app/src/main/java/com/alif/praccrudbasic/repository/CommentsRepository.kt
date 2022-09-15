a63dfa6a03b01fca09e73614e225f86ba7295e3f
SyauqiAlifI <vstudio776@gmail.com>
2022-09-15 10:02:54 +0700

    step 6 - Add and Simply Defining Repository (The bridge between UI and the Database)

--- app/src/main/java/com/alif/praccrudbasic/repository/CommentsRepository.kt
+++ app/src/main/java/com/alif/praccrudbasic/repository/CommentsRepository.kt
@@ -1,4 +1,24 @@
 package com.alif.praccrudbasic.repository
 
-class CommentsRepository {
+import com.alif.praccrudbasic.db.CommentsDAO
+import com.alif.praccrudbasic.model.Comments
+
+class CommentsRepository(private val dao: CommentsDAO) {
+    val comments = dao.getAllComments()
+
+    suspend fun insert(comments: Comments) {
+        dao.insertComments(comments)
+    }
+
+    suspend fun update(comments: Comments) {
+        dao.updateComments(comments)
+    }
+
+    suspend fun delete(comments: Comments) {
+        dao.deleteComments(comments)
+    }
+
+    suspend fun deleteAll() {
+        dao.deleteAllComments()
+    }
 }
