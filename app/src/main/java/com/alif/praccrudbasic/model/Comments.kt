ce36fe7b04a7ebf47f783a5cd133221712982333
SyauqiAlifI <vstudio776@gmail.com>
2022-09-15 09:25:45 +0700

    step 3 - Defining model (Comments)

--- app/src/main/java/com/alif/praccrudbasic/model/Comments.kt
+++ app/src/main/java/com/alif/praccrudbasic/model/Comments.kt
@@ -1,9 +1,21 @@
 package com.alif.praccrudbasic.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "tb_comments")
 data class Comments(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "commentsId")
     var id: Long,
+
+    @ColumnInfo(name = "username")
     var username: String,
+
+    @ColumnInfo(name = "email")
     var email: String,
+
+    @ColumnInfo(name = "comment")
     var comment: String
 )
