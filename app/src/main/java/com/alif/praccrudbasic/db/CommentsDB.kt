f1831cf9b9d4ee551094478f30f843b253c4cb78
SyauqiAlifI <vstudio776@gmail.com>
2022-09-15 09:58:22 +0700

    step 5 - Add and Defining Database (Used to create the database file)

--- app/src/main/java/com/alif/praccrudbasic/db/CommentsDB.kt
+++ app/src/main/java/com/alif/praccrudbasic/db/CommentsDB.kt
@@ -1,4 +1,33 @@
 package com.alif.praccrudbasic.db
 
-class CommentsDB {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.alif.praccrudbasic.model.Comments
+
+@Database(entities = [Comments::class], version = 1)
+abstract class CommentsDB: RoomDatabase() {
+    abstract val commentsDao: CommentsDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: CommentsDB? = null
+
+        fun getInstance(context: Context): CommentsDB {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        CommentsDB::class.java,
+                        "tb_comments.db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+
+    }
+
 }
