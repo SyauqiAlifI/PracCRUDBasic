6374e22de933109cbfc9d5018e8d32b53bb3a85c
SyauqiAlifI <vstudio776@gmail.com>
2022-09-15 09:43:22 +0700

    step 4 - Add and Defining Dao [Data Access Object]

--- app/src/main/java/com/alif/praccrudbasic/db/CommentsDAO.kt
+++ app/src/main/java/com/alif/praccrudbasic/db/CommentsDAO.kt
@@ -1,4 +1,23 @@
 package com.alif.praccrudbasic.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.alif.praccrudbasic.model.Comments
+
+@Dao
 interface CommentsDAO {
+    @Insert
+    suspend fun insertComments(comments: Comments): Long
+
+    @Update
+    suspend fun updateComments(comments: Comments)
+
+    @Delete
+    suspend fun deleteComments(comments: Comments)
+
+    @Query("DELETE FROM tb_comments")
+    suspend fun deleteAllComments()
+
+    @Query("SELECT * FROM tb_comments")
+    fun getAllComments(): LiveData<List<Comments>>
 }
