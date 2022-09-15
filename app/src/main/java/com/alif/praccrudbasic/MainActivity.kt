91787ce763dcc15573dd6242976f44953e7e352e
SyauqiAlifI <vstudio776@gmail.com>
2022-09-15 10:09:00 +0700

    step 8 - Configuring MainActivity.kt (added variables for binding and viewModel)

--- app/src/main/java/com/alif/praccrudbasic/MainActivity.kt
+++ app/src/main/java/com/alif/praccrudbasic/MainActivity.kt
@@ -2,10 +2,19 @@ package com.alif.praccrudbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alif.praccrudbasic.databinding.ActivityMainBinding
+import com.alif.praccrudbasic.viewmodel.MyViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
+
+    private lateinit var myViewModel: MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
